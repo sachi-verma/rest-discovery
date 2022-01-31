c58941d879bc962b0be46d461787e96f055f20ac
Sachi <sachiverma00@gmail.com>
2022-01-31 08:49:35 +0530

    updates

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const jwt = require('jsonwebtoken')
 const router = express.Router()
 const User = require('../models/user')
+const AppError = require("../utils/appError")
 
 const createToken = id => {
     return jwt.sign(
